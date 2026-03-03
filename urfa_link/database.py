24f67031d4286e2631034b731d57230405f81a8e
mehmet polat maç <mehmetmac49@gmail.com>
2026-03-03 16:49:04 +0300

    fix(db): add External PostgreSQL Support for Render deployment

--- urfa_link/database.py
+++ urfa_link/database.py
@@ -1,11 +1,18 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./urfa_link.db"
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./urfa_link.db")
 
-# connect_args={"check_same_thread": False} is required for SQLite with FastAPI
+# Fix for Render/Heroku postgres:// URLs (SQLAlchemy requires postgresql://)
+if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+# Only use check_same_thread for SQLite
+connect_args = {"check_same_thread": False} if SQLALCHEMY_DATABASE_URL.startswith("sqlite") else {}
+
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL, connect_args=connect_args
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
